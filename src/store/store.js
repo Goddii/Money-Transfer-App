e5825f42ffa55a929869861ea4c732b61d658418
Goddii <otienogodwin23@gmail.com>
2026-08-22 10:19:05 +0300

    refactor: reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
+import analyticsReducer from './slices/analyticsSlice'
+import usersReducer from './slices/usersSlice'
+import auditReducer from './slices/auditSlice'
 
 const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    analytics: analyticsReducer,
+    users: usersReducer,
+    audit: auditReducer
   }
 })
 
