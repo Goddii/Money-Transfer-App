b9c6f44115b4605a72b23131229913bd864e4fb6
lennymethu-sys <lenny.methu@student.moringaschool.com>
2026-08-25 22:29:02 +0300

    feat: connect frontend to team backend on Render

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000'
+  baseURL: 'https://money-transfer-app-backend-7gor.onrender.com'
 })
 
 api.interceptors.request.use(config => {
