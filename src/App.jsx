c1080f4d9a28a2f6f171ad3a9d3d0c33c927253e
lennymethu-sys <lenny.methu@student.moringaschool.com>
2026-08-25 22:17:21 +0300

    feat: update App.jsx with all routes - deposit, send, review, success, transactions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword'
 import Home from './pages/Home/Home'
+import Deposit from './pages/Deposit/Deposit'
+import SendMoney from './pages/SendMoney/SendMoney'
+import TransferReview from './pages/TransferReview/TransferReview'
+import TransferSuccess from './pages/TransferSuccess/TransferSuccess'
+import Transactions from './pages/Transactions/Transactions'
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 
 function App() {
@@ -15,6 +20,11 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/deposit" element={<ProtectedRoute><Deposit /></ProtectedRoute>} />
+        <Route path="/send" element={<ProtectedRoute><SendMoney /></ProtectedRoute>} />
+        <Route path="/transfer-review" element={<ProtectedRoute><TransferReview /></ProtectedRoute>} />
+        <Route path="/transfer-success" element={<ProtectedRoute><TransferSuccess /></ProtectedRoute>} />
+        <Route path="/transactions" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
         <Route path="/admin" element={<ProtectedRoute allowedRoles={['admin']}><div style={{padding:'2rem'}}>Admin - Coming soon</div></ProtectedRoute>} />
       </Routes>
     </Router>
